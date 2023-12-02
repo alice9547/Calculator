b82560fd852428599c41c27feaf3de66df7db5b7
EunSeo <alice9547@gmail.com>
2023-12-02 18:09:06 +0900

    base commit

---
+++ Calculator/Calculator/CalculatorVC.swift
@@ -0,0 +1,26 @@
+//
+//  CalculatorVC.swift
+//  Calculator
+//
+//  Created by 김은서 on 12/2/23.
+//
+
+import UIKit
+
+class CalculatorVC: UIViewController {
+    
+    private let logoView = LogoView()
+    private let resultView = ResultView()
+    private let billInputView = BillInputView()
+    private let tipInputView = TipInputView()
+    private let splitInputView = SplitInputView()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
